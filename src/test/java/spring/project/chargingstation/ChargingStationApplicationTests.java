4dabd9c19bf0f1c12b8c28dc233ac7b935017eaa
yslee96 <dbstn322@gmail.com>
2023-02-04 14:21:36 +0900

    first commit

---
+++ src/test/java/spring/project/chargingstation/ChargingStationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.chargingstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
